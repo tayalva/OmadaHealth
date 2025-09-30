3a99a834c28387addd00ae3a1515cc4fc677d889
Taylor Smith <taylor.smith@hatchbaby.com>
2025-09-30 09:28:41 -0600

    Initial Commit

---
+++ OmadaProject/OmadaProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  OmadaProjectApp.swift
+//  OmadaProject
+//
+//  Created by Taylor Smith on 9/30/25.
+//
+
+import SwiftUI
+
+@main
+struct OmadaProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
