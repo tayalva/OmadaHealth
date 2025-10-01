fb1710cadd67ef43abf4f4eaf743c2176113479a
Taylor Smith <taylor.smith@hatchbaby.com>
2025-10-01 11:37:12 -0600

    made custom search bar

--- OmadaProject/MovieSearch/MovieSearchView.swift
+++ OmadaProject/MovieSearch/MovieSearchView.swift
@@ -13,6 +13,9 @@ struct MovieSearchView: View {
     
     var body: some View {
         NavigationStack(path: $viewModel.navPath) {
+            
+            CustomSearchBar(text: $searchText)
+            
                 ScrollView {
                     LazyVStack(alignment: .leading) {
                         ForEach(Array(viewModel.results).enumerated(), id: \.offset) { index, movie in
@@ -40,7 +43,6 @@ struct MovieSearchView: View {
                     MovieDetailView(movie: movie)
                 }
             }
-        .searchable(text: $searchText, placement: .navigationBarDrawer)
           
         .onChange(of: searchText) {
             Task {
@@ -81,5 +83,44 @@ extension MovieSearchView {
         func goToDetailView(for movie: Movie) {
             navPath.append(movie)
         }
+    }
+}
+
+import SwiftUI
+
+struct CustomSearchBar: View {
+    @Binding var text: String
+    @State private var isEditing = false
+
+    var body: some View {
+        HStack {
+            TextField("Search", text: $text)
+                .padding(8)
+                .padding(.horizontal, 25)
+                .background(Color(.systemGray6))
+                .cornerRadius(8)
+                .overlay(
+                    HStack {
+                        Image(systemName: "magnifyingglass")
+                            .foregroundColor(.gray)
+                            .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
+                            .padding(.leading, 8)
+
+                        if isEditing && !text.isEmpty {
+                            Button(action: {
+                                self.text = ""
+                            }) {
+                                Image(systemName: "multiply.circle.fill")
+                                    .foregroundColor(.gray)
+                                    .padding(.trailing, 8)
+                            }
+                        }
+                    }
+                )
+                .onTapGesture {
+                    self.isEditing = true
+                }
+        }
+        .padding(.horizontal)
     }
 }
